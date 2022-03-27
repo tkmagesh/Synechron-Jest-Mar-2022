b0b47be1547207f064527b67bf394b13b4365d7a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-27 10:21:14 +0530

    updates

--- first-app/mock-class/greeter-greet-2.spec.js
+++ first-app/mock-class/greeter-greet-2.spec.js
@@ -1,73 +1,38 @@
 const Greeter = require('./greeter')
 const DateService = require('./dateService');
-jest.mock('./dateService');
 
-describe("Greeter", () => {
-
-    describe('Greeter - Afternoon', () => {
-        beforeEach(() => {
-            DateService.mockImplementationOnce(() => {
-                return { //object returned when a call to 'new DateService()' is made
-                    getCurrent : jest.fn().mockReturnValue(new Date('27-Mar-2022 14:00:00'))
-                }
-            })
-        })
-        it('Should greet with "Good Afternoon" when greeted after 12', () => {
-        
-            const sut = new Greeter(),
-                userName = 'Magesh',
-                expectedResult = `Hi Magesh, Good Afternoon`;
-
-            //Act
-            const actualResult = sut.greet(userName)
-
-            //Assert
-            expect(DateService).toHaveBeenCalled()
-            expect(actualResult).toBe(expectedResult)
-            
-            /* const mockDateService = DateService.mock.instances[0]
-            
-            expect(mockDateService.getCurrent).toHaveBeenCalled() */
-        })
-
+const mockGetCurrentTime = jest.fn().mockReturnValue(new Date('27-Mar-2022 14:00:00'))
+jest.mock('./dateService', () => {
+    return jest.fn().mockImplementation(() => {
+        return { //object returned when a call to 'new DateService()' is made
+            getCurrent : mockGetCurrentTime
+        }
     })
+});
+
+describe("Greeter - Afternoon", () => {
+    beforeEach(() => {
+        DateService.mockClear()
+        mockGetCurrentTime.mockClear()
+    });
+
+    it('Should greet with "Good Afternoon" when greeted after 12', () => {
     
-     describe('Greeter - Morning', () => {
-        beforeAll(() => {
-             DateService.mockImplementation(() => {
-                return {
-                    getCurrent : () => { 
-                        return new Date('27-Mar-2022 9:00:00')
-                    }
-                }
-            })
-        })
-        it('Should greet with "Good Morning" when greeted at 9', () => {
-            
-            const sut = new Greeter(),
-                userName = 'Magesh',
-                expectedResult = `Hi Magesh, Good Morning`;
+        const sut = new Greeter(),
+            userName = 'Magesh',
+            expectedResult = `Hi Magesh, Good Afternoon`;
 
-            //Act
-            const actualResult = sut.greet(userName)
+        //Act
+        const actualResult = sut.greet(userName)
 
-            //Assert
-            expect(actualResult).toBe(expectedResult)
-            expect(DateService).toHaveBeenCalled()
-        })
-
-        it('Should greet with "Good Morning" when greeted before 12', () => {
+        //Assert
+        //expect(DateService).toHaveBeenCalled()
+        expect(actualResult).toBe(expectedResult)
+        expect(mockGetCurrentTime).toHaveBeenCalled()
         
-            const sut = new Greeter(),
-                userName = 'Magesh',
-                expectedResult = `Hi Magesh, Good Morning`;
-
-            //Act
-            const actualResult = sut.greet(userName)
-
-            //Assert
-            expect(actualResult).toBe(expectedResult)
-            expect(DateService).toHaveBeenCalled()
-        })
-     })
+        
+        /* const mockDateService = DateService.mock.instances[0]
+        
+        expect(mockDateService.getCurrent).toHaveBeenCalled() */
+    })
 })
